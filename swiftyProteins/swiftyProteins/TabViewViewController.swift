6bad4747cc4ffd4e4c0c03d938e061ce4cda0cab
Yaroslava HLIBOCHKO <yhliboch@e1r9p16.unit.ua>
2019-07-17 21:24:03 +0300

    search bar done

--- swiftyProteins/swiftyProteins/TabViewViewController.swift
+++ swiftyProteins/swiftyProteins/TabViewViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class TabViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TabViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     
     var ligands: [String] = []
+    var filterLigands: [String] = []
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
         if let path = Bundle.main.path(forResource: "ligands", ofType: "txt") {
             do {
                 let data = try String(contentsOfFile: path, encoding: .utf8)
@@ -26,19 +29,29 @@ class TabViewViewController: UIViewController, UITableViewDelegate, UITableViewD
                     myStrings.removeLast()
                 }
                 ligands = myStrings
+                filterLigands = ligands
             } catch {
                 print(error)
             }
         }
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterLigands = searchText.isEmpty ? ligands : ligands.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ligands.count
+        return filterLigands.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? LigandTableViewCell {
-            cell.titleLabel.text = ligands[indexPath.row]
+            cell.titleLabel.text = filterLigands[indexPath.row]
             return cell
         } else {
             return UITableViewCell()
